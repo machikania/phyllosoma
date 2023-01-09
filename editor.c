ca4e86727a4e5865d24ac6ada5551c5cb0d5e63b
KenKenMkIISR <tanaka-kenji@ll.em-net.ne.jp>
2023-01-09 14:27:03 +0900

    optimize editor redraw

--- editor.c
+++ editor.c
@@ -507,8 +507,10 @@ void redraw(){
 	int ix,ix1,ix2;
 	int x,y;
 	unsigned char ch,cl;
+	unsigned char written;
 
 	vp=TVRAM;
+	written=0;
 	bp=disptopbp;
 	ix=disptopix;
 	cl=COLOR_NORMALTEXT;
@@ -553,23 +555,35 @@ void redraw(){
 			if(bp==bp2 && ix==ix2) cl=COLOR_NORMALTEXT;
 			ch=bp->Buf[ix++];
 			if(ch=='\n') break;
-			*(vp+ATTROFFSET)=cl;
-			*vp++=ch;
+			if(*vp!=ch || *(vp+ATTROFFSET)!=cl){
+				*vp=ch;
+				*(vp+ATTROFFSET)=cl;
+				written=1;
+			}
+			vp++;
 		}
 		//改行およびバッファ最終以降の右側表示消去
 		for(;x<WIDTH_X;x++){
-			*(vp+ATTROFFSET)=0;
-			*vp++=0;
+			if(*vp!=0 || *(vp+ATTROFFSET)!=0){
+				*vp=0;
+				*(vp+ATTROFFSET)=0;
+				written=1;
+			}
+			vp++;
 		}
 	}
 	//バッファ最終以降の下側表示消去
 	for(;y<EDITWIDTHY;y++){
 		for(x=0;x<WIDTH_X;x++){
-			*(vp+ATTROFFSET)=0;
-			*vp++=0;
+			if(*vp!=0 || *(vp+ATTROFFSET)!=0){
+				*vp=0;
+				*(vp+ATTROFFSET)=0;
+				written=1;
+			}
+			vp++;
 		}
 	}
-	textredraw();
+	if(written) textredraw(); //液晶に出力
 }
 
 //カーソルを1つ前に移動
@@ -1892,6 +1906,7 @@ void changewidth(void){
 	EDITWIDTHY=WIDTH_Y-1; //エディタ画面行数設定
 	cursor_top(); //カーソルをテキストバッファの先頭に設定
 	redraw(); //再描画
+	textredraw(); //液晶に強制出力
 }
 
 //KM-BASICコンパイル＆実行
