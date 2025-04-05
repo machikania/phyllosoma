6482f118f38ebcecf6a78ee1e2157b448295d462
Katsumi <dummy@rad51.net>
2025-04-04 19:43:09 -0700

    Ver 1.5.2 KM-1509

--- config.h
+++ config.h
@@ -32,8 +32,8 @@
 #define RP2350 0
 #endif
 
-#define SYSVER2 "1.5.1.0"
-#define BASVER "KM-1508"
+#define SYSVER2 "1.5.2.0"
+#define BASVER "KM-1509"
 
 #define INTRODUCE_MACHIKANIA \
 	"MachiKania BASIC System\n"\
