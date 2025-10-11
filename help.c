2746feae30edef2d5550146a78df33d2001c4154
Katsumi <dummy@rad51.net>
2025-10-11 11:28:34 -0700

    Support functions in editor help.

--- help.c
+++ help.c
@@ -12,24 +12,10 @@ char g_help_file[64]="/docume~1/help-e.txt";
 
 char* get_help(const char* word){
 	// Longest words are I2CWRITEDATA/SPIWRITEDATA (12 bytes)
-	char uword[13];
 	int i,j;
 	char* ret;
 	FIL fpo;
 	FIL* fp=&fpo;
-	// Seek the first character
-	while('A'<=word[-1] && word[-1]<='Z' || 'a'<=word[-1] && word[-1]<='z' || '0'<=word[-1] && word[-1]<='9') word--;
-	// Convert to uppercases
-	for(i=0;i<12;i++){
-		if ('A'<=word[i] && word[i]<='Z' || '0'<=word[i] && word[i]<='9') {
-			uword[i]=word[i];
-		} else if ('a'<=word[i] && word[i]<='z') {
-			uword[i]=word[i]-0x20;
-		} else {
-			break;
-		}
-	}
-	uword[i]=0;
 	// Open the help file
 	if (f_open(fp,g_help_file,FA_READ)) return 0;
 	// Search the help file
@@ -37,16 +23,22 @@ char* get_help(const char* word){
 		if (!f_gets(g_file_buffer, g_file_buffer_size, fp)) continue;
 		// Check the string
 		for(i=0;i<12;i++){
-			if (uword[i]!=g_file_buffer[i]) break;
+			if (word[i]!=g_file_buffer[i]) break;
 		}
-		if (0x00!=uword[i]) continue;
-		if (0x20!=g_file_buffer[i] && 0x0d!=g_file_buffer[i] && 0x0a!=g_file_buffer[i]) continue;
-		// The word found. Add the next line
-		for(i=0;g_file_buffer[i] && i<g_file_buffer_size-1;i++); // Goto \x0
-		f_gets(&g_file_buffer[i], g_file_buffer_size-i, fp);
-		// Close the file and return pointer
-		f_close(fp);
-		return g_file_buffer;
+		if (0x00!=word[i]) continue;
+		switch(g_file_buffer[i]){
+			case 0x20: case 0x0d: case 0x0a:
+			case '(': case '$': case '#':
+				// The word found. Add the next line
+				for(i=0;g_file_buffer[i] && i<g_file_buffer_size-1;i++); // Goto \x0
+				f_gets(&g_file_buffer[i], g_file_buffer_size-i, fp);
+				// Close the file and return pointer
+				f_close(fp);
+				return g_file_buffer;
+			default:
+				// The word not yet found
+				break;
+		}
 	}
 	// The word not found
 	// Close the file and return null
