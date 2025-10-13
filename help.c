381ecbbe99243d9b8e5d8ce8a8013559556510fb
Katsumi <dummy@rad51.net>
2025-10-13 00:17:06 -0700

    Support multiple functions assigned to a word in help.c

--- help.c
+++ help.c
@@ -26,7 +26,11 @@ int ini_file_help(char* line){
 
 char* get_help(const char* word){
 	// Longest words are I2CWRITEDATA/SPIWRITEDATA (12 bytes)
-	int i,num_lines;
+	int i;
+	int num_lines; // Number of lines in each sentence
+	int num_funcs; // Number of functions assigned to the word
+	char* buffer=g_file_buffer;
+	int buffer_size=g_file_buffer_size;
 	unsigned int hash;
 	FIL fpo;
 	FIL* fp=&fpo;
@@ -34,6 +38,7 @@ char* get_help(const char* word){
 	hash=cmpdata_nhash(word,0);
 	// Special cases
 	num_lines=1;
+	num_funcs=1;
 	switch(hash){
 		case 0x00047392: //FOR
 		case 0x0000154f: //TO
@@ -67,12 +72,22 @@ char* get_help(const char* word){
 		case 0x130576fa: //INTERRUPT
 		case 0x47204344: //FIELD
 		case 0x95015217: //STATIC
-			num_lines=2;
-			break;
+		case 0x4748410b: //FSEEK
 			num_lines=2;
 			break;
 		case 0x98495119: //SYSTEM
 			num_lines=3;
+			break;
+		case 0x827c002e: //DRAWCOUNT
+		case 0x4c512203: //MUSIC
+		case 0x18594115: //PLAYWAVE
+		case 0x8238d383: //GCOLOR
+		case 0xea9ef7dc: //CORETIMER
+		case 0x5520c112: //TIMER
+			num_funcs=2;
+			break;
+		case 0x46392502: //GOSUB
+			num_funcs=4;
 			break;
 		case 0x00000041: //A
 		case 0x00059392: //XOR
@@ -92,28 +107,41 @@ char* get_help(const char* word){
 	}
 	// Search the help file
 	while(!f_eof(fp)){
-		if (!f_gets(g_file_buffer, g_file_buffer_size, fp)) continue;
+		if (!f_gets(buffer, buffer_size, fp)) continue;
 		// Check the string
 		for(i=0;i<12;i++){
-			if (word[i]!=g_file_buffer[i]) break;
+			if (word[i]!=buffer[i]) break;
 		}
 		if (0x00!=word[i]) continue;
-		switch(g_file_buffer[i]){
+		switch(buffer[i]){
 			case 0x20: case 0x0d: case 0x0a:
 			case '(': case '$': case '#':
 				// The word found. Add the next line
-				for(i=0;g_file_buffer[i] && i<g_file_buffer_size-1;i++); // Goto \x0
-				while(i<g_file_buffer_size-1 && !f_eof(fp)) {
-					f_gets(&g_file_buffer[i], g_file_buffer_size-i, fp);
-					if ('\t'!=g_file_buffer[i]) {
+				for(i=0;buffer[i] && i<buffer_size-1;i++); // Goto \x0
+				while(i<buffer_size-1 && !f_eof(fp)) {
+					f_gets(&buffer[i], buffer_size-i, fp);
+					if ('\t'!=buffer[i]) {
 						// Next statement description started
 						if ((--num_lines)<1) break;
 					}
 					// Statement description continued
-					for(i=i;g_file_buffer[i] && i<g_file_buffer_size-1;i++); // Goto \x0
+					for(i=i;buffer[i] && i<buffer_size-1;i++); // Goto \x0
 				}
-				g_file_buffer[i]=0x00;
-				if (g_file_buffer_size-1<=i) g_file_buffer[i-3]=g_file_buffer[i-2]=g_file_buffer[i-1]='.'; // Exceeds 1024 bytes
+				buffer[i]=0x00;
+				if (buffer_size-1<=i) {
+					// Exceeds 1024 bytes
+					buffer[i-3]=buffer[i-2]=buffer[i-1]='.';
+					// Close the file and return pointer
+					f_close(fp);
+					return g_file_buffer;
+				}
+				if (0<(--num_funcs)) {
+					// There is/are still text(s) to show
+					buffer[i++]=0x0a;
+					buffer+=i;
+					buffer_size-=i;
+					continue;
+				}
 				// Close the file and return pointer
 				f_close(fp);
 				return g_file_buffer;
